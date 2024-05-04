ab7a63c2052d049869934d735e9bf240b7f82d2b
misung.dev <misung.dev@gmail.com>
2024-05-05 00:52:14 +0900

    Solve: 후위 연산식 (2차 풀이)

--- section6/4-postfix.js
+++ section6/4-postfix.js
@@ -1,27 +1,64 @@
-function solution(s) {
-	let answer;
-	let stack = [];
+// solution1
+function solution1(s) {
+	const stack = [];
 
-	for (let x of s) {
-		if (!isNaN(x)) {
-			stack.push(Number(x));
-		} else {
-			let rt = stack.pop();
-			let lt = stack.pop();
+	for (const x of s) {
+		if (isNaN(x)) {
+			// 숫자 아니면, 숫자 두개를 꺼내와서 pop
+			const right = stack.pop();
+			const left = stack.pop();
+
 			if (x === "+") {
-				stack.push(lt + rt);
+				stack.push(left + right);
 			} else if (x === "-") {
-				stack.push(lt - rt);
+				stack.push(left - right);
 			} else if (x === "*") {
-				stack.push(lt * rt);
-			} else if (x === "/") {
-				stack.push(lt / rt);
+				stack.push(left * right);
+			} else {
+				stack.push(left / right);
 			}
+		} else {
+			stack.push(Number(x));
 		}
 	}
-	answer = stack[0];
 
-	return answer;
+	return stack[0];
+}
+
+// solution2
+const calculate = (num1, num2, operator) => {
+	switch (operator) {
+		case "+":
+			return num1 + num2;
+
+		case "-":
+			return num1 - num2;
+
+		case "*":
+			return num1 * num2;
+
+		case "/":
+			return num1 / num2;
+	}
+};
+
+function solution(s) {
+	const stack = [];
+
+	[...s].forEach((element) => {
+		if (isNaN(element)) {
+			const [num2, num1] = [stack.pop(), stack.pop()].map(Number);
+			// const num2 = Number(stack.pop());
+			// const num1 = Number(stack.pop());
+			const result = calculate(num1, num2, element);
+
+			stack.push(result);
+		} else {
+			stack.push(element);
+		}
+	});
+
+	return stack[0];
 }
 
 // 테스트 케이스
